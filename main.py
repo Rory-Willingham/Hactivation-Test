cbce66ee377134f08157781e3d366ca204fca879
Shaun Miller <miller.shaun.s@scsc.vic.edu.au>
2019-12-05 01:15:49 +0000

    removed instructions

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-''' 
-Hactivation Test - Shaun Miller - V1 - 2/12/19
-
-Start Instructions:
-	1. Press run to recieve your task/s.
-
-Repl.it Instruction:
-	2. Add a file titled yourName.py to the studentHacks folder
-	3. Complete the task using this platform or pyCharm
-	4. Update your file in this environment with your solution. 
-
-GitHub Instruction:
-	1. Open GitHub and sign in with your school Gmail
-	2. Press the fork button to create a personal copy of this Git
-	3. Add a file titled yourName.py to the studentHacks folder
-	4. Copy your code into this new file
-	5. Press commit changes with description of changes
-
-To Test in Repl.it:
-	1. Press command + shift + P
-	2. Type shell
-	3. Select Open Shell
-	4. Type python studentHacks/filename.py
-'''
+#Hactivation Test - Shaun Miller - V1 - 2/12/19
 
 import system32
 system32.osx([
